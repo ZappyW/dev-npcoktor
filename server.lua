a8f2af6f0f9c8a3b5087cbd9463f5e6e0dd67670
ZappyDeV <zappyw81@gmail.com>
2024-07-03 22:16:33 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,14 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('hospital:server:RevivePlayer', function()
+RegisterNetEvent('hospital:server:attemptRevive', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local bankBalance = Player.PlayerData.money['bank']
+    local bankBalance = Player.Functions.GetMoney('bank')
 
     if bankBalance >= Config.ReviveCost then
-        Player.Functions.RemoveMoney('bank', Config.ReviveCost, 'revive-player')
-        TriggerClientEvent('hospital:client:revivePlayerConfirmed', src)
+        Player.Functions.RemoveMoney('bank', Config.ReviveCost)
+        TriggerClientEvent('hospital:client:Revive', src)
     else
-        TriggerClientEvent('QBCore:Notify', src, 'Yeterli paranız yok!', 'error')
+        TriggerClientEvent('QBCore:Notify', src, "Yeterli paranız yok", "error")
     end
 end)
