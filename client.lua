e6cb42e9ae1a432cfd37ac3ac151fc5d19c976d5
ZappyDeV <zappyw81@gmail.com>
2025-12-10 20:25:25 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,39 +1,89 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-------------------------------------------------------
+-- PROGRESS SYSTEM WRAPPER (Config'e göre çalışır)
+-------------------------------------------------------
+local function StartProgress(label, time, onFinish, onCancel)
+    if Config.ProgressSystem == "qb-core" then
+        QBCore.Functions.Progressbar("revive_player", label, time, false, true, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+        }, {}, {}, {}, function()
+            if onFinish then onFinish() end
+        end, function()
+            if onCancel then onCancel() end
+        end)
+
+    elseif Config.ProgressSystem == "ox-lib" then
+        local success = lib.progressCircle({
+            duration = time,
+            position = 'bottom',
+            label = label,
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                move = true,
+                car = true,
+                combat = true,
+            },
+        })
+
+        if success then
+            if onFinish then onFinish() end
+        else
+            if onCancel then onCancel() end
+        end
+    end
+end
+
+-------------------------------------------------------
+-- NPC SPAWN FONKSİYONU
+-------------------------------------------------------
 local function spawnNPCs()
     for _, npc in ipairs(Config.NPCs) do
         RequestModel(GetHashKey(npc.model))
         while not HasModelLoaded(GetHashKey(npc.model)) do
-            Wait(1)
+            Wait(10)
         end
-        local npcEntity = CreatePed(4, GetHashKey(npc.model), npc.coords.x, npc.coords.y, npc.coords.z - 1.0, npc.heading, false, true)
+
+        local npcEntity = CreatePed(
+            4,
+            GetHashKey(npc.model),
+            npc.coords.x, npc.coords.y, npc.coords.z - 1.0,
+            npc.heading,
+            false, true
+        )
+
         FreezeEntityPosition(npcEntity, true)
         SetEntityInvincible(npcEntity, true)
         SetBlockingOfNonTemporaryEvents(npcEntity, true)
         SetPedCanRagdoll(npcEntity, false)
 
+        -------------------------------------------------------
+        -- TARGET SİSTEM SEÇİMİ
+        -------------------------------------------------------
         if Config.TargetSystem == "qb-target" then
             exports['qb-target']:AddTargetEntity(npcEntity, {
                 options = {
                     {
                         type = "client",
-                        event = "hospital:client:startRevive",
                         icon = "fas fa-heartbeat",
                         label = "Tedavi Ol",
-                        action = function(entity)
-                            TriggerEvent("hospital:client:startRevive")
-                        end
-                    },
+                        event = "hospital:client:startRevive",
+                    }
                 },
                 distance = 2.5,
             })
+
         elseif Config.TargetSystem == "ox-target" then
             exports.ox_target:addLocalEntity(npcEntity, {
                 {
                     name = 'revive_npc',
                     icon = 'fas fa-heartbeat',
                     label = 'Tedavi Ol',
-                    onSelect = function(data)
+                    onSelect = function()
                         TriggerEvent('hospital:client:startRevive')
                     end,
                 }
@@ -42,47 +92,56 @@ local function spawnNPCs()
     end
 end
 
+-------------------------------------------------------
+-- REVIVE FONKSİYONU
+-------------------------------------------------------
 local function RevivePlayer(playerPed)
     SetEntityHealth(playerPed, GetEntityMaxHealth(playerPed))
     ClearPedTasksImmediately(playerPed)
     SetEntityCoords(playerPed, GetEntityCoords(playerPed))
 end
 
+-------------------------------------------------------
+-- REVIVE EVENTLERİ
+-------------------------------------------------------
 RegisterNetEvent('hospital:client:startRevive', function()
-    QBCore.Functions.Progressbar("revive_player", "Tedavi Oluyor...", 10000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true,
-    }, {}, {}, {}, function() -- Done
-        TriggerServerEvent('hospital:server:attemptRevive')
-    end, function() -- Cancel
-        QBCore.Functions.Notify("Tedavi İptal Edildi", "error")
-    end)
+    StartProgress(
+        "Tedavi Oluyor...",
+        10000,
+        function()  -- On Finish
+            TriggerServerEvent('hospital:server:attemptRevive')
+        end,
+        function()  -- On Cancel
+            QBCore.Functions.Notify("Tedavi İptal Edildi", "error")
+        end
+    )
 end)
 
 RegisterNetEvent('hospital:client:Revive', function()
-    local playerPed = PlayerPedId()
-    RevivePlayer(playerPed)
+    RevivePlayer(PlayerPedId())
 end)
 
+-------------------------------------------------------
+-- RESOURCE BAŞLARKEN NPC OLUŞTURMA
+-------------------------------------------------------
 AddEventHandler('onResourceStart', function(resourceName)
     if GetCurrentResourceName() == resourceName then
         spawnNPCs()
     end
 end)
 
--- Alternatif olarak "onClientMapStart" eventi de kullanılabilir
 AddEventHandler('onClientMapStart', function()
     spawnNPCs()
 end)
 
--- NPC'lerin her zaman invincible olmasını sağlamak için ekstra bir kontrol
+-------------------------------------------------------
+-- NPC INVINCIBLE KORUMA (5 saniyede bir)
+-------------------------------------------------------
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(5000) -- 5 saniyede bir kontrol
+        Citizen.Wait(5000)
         for _, npc in ipairs(Config.NPCs) do
-            local npcEntity = GetClosestPed(npc.coords.x, npc.coords.y, npc.coords.z, 1.0, 0, 0, 0, 0, -1)
+            local npcEntity = GetClosestPed(npc.coords.x, npc.coords.y, npc.coords.z, 1.5, false, false, false, false, -1)
             if npcEntity ~= 0 then
                 FreezeEntityPosition(npcEntity, true)
                 SetEntityInvincible(npcEntity, true)
