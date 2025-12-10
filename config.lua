9db05a21282b8e93e7df26c0623cc1a2424f276d
ZappyDeV <zappyw81@gmail.com>
2025-12-10 20:24:29 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,9 +2,9 @@ Config = {}
 
 Config.NPCs = {
     {coords = vector3(321.93, -590.29, 43.28), heading = 152.42, model = 's_m_m_doctor_01'},
-   
 }
 
-Config.TargetSystem = "ox-target" -- qb-taget ya da "ox-target"
+Config.TargetSystem = "ox-target"   -- "qb-target" / "ox-target"
+Config.ProgressSystem = "ox-lib"    -- "qb-core" / "ox-lib"
 
-Config.ReviveCost = 500 
+Config.ReviveCost = 500
